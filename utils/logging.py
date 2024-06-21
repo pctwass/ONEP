3f7b88d7446cd9c29ed647cf3699ea49d24991c6
pctwass <peterw10001@gmail.com>
2024-06-21 12:06:39 +0200

    temporarly use default logging

--- utils/logging.py
+++ utils/logging.py
@@ -1,3 +1,5 @@
-from dareplane_utils.logging.logger import get_logger
+# from dareplane_utils.logging.logger import get_logger
+import logging as py_logging
 
-logger = get_logger("ONEP")
+# logger = get_logger("ONEP")
+logger = py_logging.getLogger("ONEP")
