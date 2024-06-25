624a618d77b86f70a058ac3bb14b13e456d4fe38
pctwass <peterw10001@gmail.com>
2024-06-25 13:48:19 +0200

    toggle connecting to stream when starting process

--- process_management/projector_processes.py
+++ process_management/projector_processes.py
@@ -11,11 +11,13 @@ from utils.data_mocker import get_mock_data
 def create_living_process_project(projector : Projector, stream_watcher : StreamWatcher, flags : dict[str, multiprocessing.Event], use_mock_data : bool = False) -> multiprocessing.Process:
     if use_mock_data:
         reader_function = get_mock_data
+        connect_to_stream = False
     else:
         reader_function = stream_watcher.read
+        connect_to_stream = True
 
     process_target = _projecting_loop
-    kwargs = dict(projector=projector, stream_watcher=stream_watcher, reader_function=reader_function, flags=flags)
+    kwargs = dict(projector=projector, stream_watcher=stream_watcher, reader_function=reader_function, flags=flags, connect_to_stream=connect_to_stream)
     subprocess = create_subprocess(process_target, kwargs=kwargs)
 
     return subprocess
@@ -33,14 +35,16 @@ def _projecting_loop(
     projector : Projector,
     stream_watcher : StreamWatcher,
     reader_function,
-    flags : dict[str, multiprocessing.Event] = {}
+    flags : dict[str, multiprocessing.Event] = {},
+    connect_to_stream = True
     ):
 
     freq_hz = projector._getvalue()._settings.sampling_frequency
     dt = 1 / freq_hz
     tlast = time.time_ns()
 
-    stream_watcher.connect_to_streams()
+    if connect_to_stream:
+        stream_watcher.connect_to_streams()
 
     while not flags["stop"].is_set():
         now = time.time_ns()
